b728640a3cd5600ba354cc11d9b50943081e76cd
marcos pinardi <exegeses@gmail.com>
2020-08-14 15:37:27 -0300

    ejercicio fecha

--- clase2/js/fecha.js
+++ clase2/js/fecha.js
@@ -1,1 +1,17 @@
-    document.write('funciona!');
+
+// ubicamos elemento dentro del DOM
+    var txtFecha = document.getElementById('txtFecha');
+
+//creamos un objeto de fecha
+    var fecha = new Date();
+
+// obtenemos dia/mes/anio
+    var diaMes = fecha.getDate();
+    var mes = fecha.getMonth() + 1;
+    if ( mes < 10 ){
+        mes = '0'+mes;
+    }
+    var anio = fecha.getFullYear();
+
+//mostramos la fecha en el span txtFecha  ej: 14/08/2020
+    txtFecha.innerText = diaMes +'/'+ mes +'/'+ anio;
