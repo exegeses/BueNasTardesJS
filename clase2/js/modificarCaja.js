2c89b8fb945361ef665ec9d32452fe095dddb1df
marcos pinardi <exegeses@gmail.com>
2020-08-14 16:01:33 -0300

    crear una función

---
+++ clase2/js/modificarCaja.js
@@ -0,0 +1,11 @@
+// ubicar elemento dentro del DOM
+    var caja = document.getElementById('caja');
+
+// modificamos atributos
+
+    function modificar()
+    {
+        caja.style.backgroundColor = '#dea16a';
+        caja.style.width = '150px';
+        caja.style.height = '150px';
+    }
