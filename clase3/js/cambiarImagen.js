650db95726bbe9d772937e860237a9f0d399a0ea
marcos pinardi <exegeses@gmail.com>
2020-08-19 16:00:21 -0300

    tarea cambiar imagen al ponerl el mouse sobre

---
+++ clase3/js/cambiarImagen.js
@@ -0,0 +1,9 @@
+// ubicar elemento dentro del DOM
+
+// generar funcion/es de control
+
+    //document.getElementById('logo').src = 'logos/logocss.png'
+
+/* #consigna:
+*   cambiar la imagen al poner el mouse sobre el link
+*  */
