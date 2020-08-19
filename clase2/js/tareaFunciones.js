4b3bd1a8d179405451baaaf3470f240df56c881b
marcos pinardi <exegeses@gmail.com>
2020-08-19 14:37:32 -0300

    tarea colorear usando funciones

---
+++ clase2/js/tareaFunciones.js
@@ -0,0 +1,22 @@
+// ubicar elemento/s dentro del DOM
+    var caja = document.getElementById('caja');
+
+// generar función/es
+    function verdeAR()
+    {
+        caja.style.backgroundColor = '#84e594';
+        caja.innerText = 'verde';
+        caja.style.color = '#0b6918';
+    }
+    function naranjeAR()
+    {
+        caja.style.backgroundColor = '#e5be84';
+        caja.innerText = 'naranja';
+        caja.style.color = '#693c0b';
+    }
+    function celesteAR()
+    {
+        caja.style.backgroundColor = '#84e5dd';
+        caja.innerText = 'celeste';
+        caja.style.color = '#0b6669';
+    }
