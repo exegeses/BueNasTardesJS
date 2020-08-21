e673dd9aecba9724463543d6a702e05ac327d778
marcos pinardi <exegeses@gmail.com>
2020-08-21 15:54:02 -0300

    ejercicio de mostrar/ocultar caja

---
+++ clase4/js/mostarOcultar.js
@@ -0,0 +1,17 @@
+
+    var caja = document.getElementById('caja');
+
+    function ocultar()
+    {
+        // display = 'none'
+        // visibility = 'hidden'
+        // opacity = '0';
+        caja.style.display = 'none';
+    }
+    function mostrar()
+    {
+        // display = 'block'
+        // visibility = 'visible'
+        // opacity = 1;
+        caja.style.display = 'block';
+    }
