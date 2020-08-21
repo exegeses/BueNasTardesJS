393641c04ea8413635625646716877af0709fd2d
marcos pinardi <exegeses@gmail.com>
2020-08-21 14:18:34 -0300

    tarea cambiar imagen resuelta

--- clase3/js/cambiarImagen.js
+++ clase3/js/cambiarImagen.js
@@ -1,8 +1,18 @@
 // ubicar elemento dentro del DOM
-
+    var logo = document.getElementById('logo')
 // generar funcion/es de control
 
-    //document.getElementById('logo').src = 'logos/logocss.png'
+    function html()
+    {
+        logo.src = 'logos/logohtml.png';
+    }
+    function css()
+    {
+        logo.src = 'logos/logocss.png';
+    }
+    function js() {
+        logo.src = 'logos/logojs.png';
+    }
 
 /* #consigna:
 *   cambiar la imagen al poner el mouse sobre el link
