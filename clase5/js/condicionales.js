856c2b8d3b8027d1dd73b34d6529ecc9d2d791e6
marcos pinardi <exegeses@gmail.com>
2020-08-26 14:43:48 -0300

    dudas sobre condicional if/else

--- clase5/js/condicionales.js
+++ clase5/js/condicionales.js
@@ -1,6 +1,15 @@
+    var mensaje = document.getElementById('mensaje');
 
     function comparar()
     {
         var numero = prompt('Ingrese un número');
-
+        if ( numero < 100 ){
+            // ventana emergente alert()
+            //alert('Es menor que 100');
+            mensaje.innerText = 'Es menor que 100';
+        }
+        else {
+            //alert('No es menor que 100');
+            mensaje.innerText = 'No es menor que 100'
+        }
     }
