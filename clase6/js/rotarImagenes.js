265e5548a320f558c3e81024f1f79a88e85ab227
marcos pinardi <exegeses@gmail.com>
2020-08-28 16:01:37 -0300

    tarear rotar imagenes

---
+++ clase6/js/rotarImagenes.js
@@ -0,0 +1,16 @@
+
+    var foto = document.getElementById('foto');
+
+    //crear un array con los nombres de las imágenes
+    var fotos = [
+                'arq1.jpg', 'arq2.jpg', 'arq3.jpg',
+                'dancer1.jpg', 'dancer2.jpg', 'dancer3.jpg',
+                'moda1.jpg', 'moda2.jpg', 'moda3.jpg',
+                ];
+
+    // crear a función para rotar imagenes
+
+    //obtener un número aleatorio entre 0 y 8
+    // el número debe ser entero
+
+    //con un botón mostrar imagen al azar
