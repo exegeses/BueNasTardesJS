d65b537dffb7f0c724b94d90197f30e1b3149afa
marcos pinardi <exegeses@gmail.com>
2020-09-02 14:40:37 -0300

    ubicar elemento usando getElementsByTagName()

---
+++ clase7/js/ubicarElementos.js
@@ -0,0 +1,11 @@
+// maneras de ubicar elementos
+
+    var txtFrase = document.getElementById('txtFrase');
+        txtFrase.innerText = 'Elemento encontrado!'
+
+    var losI = document.getElementsByTagName('i');
+        console.log(losI);
+    //variamos color de texto
+        losI[2].style.backgroundColor = '#f1985e';
+        losI[2].style.padding = '3px 6px'
+        losI[2].style.border = '1px dashed #f60';
