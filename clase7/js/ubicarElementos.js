a91360747636df1613b86cb6cca625e6802252de
marcos pinardi <exegeses@gmail.com>
2020-09-02 14:59:34 -0300

    ubicar elemento usando getElementsByClassName()

--- clase7/js/ubicarElementos.js
+++ clase7/js/ubicarElementos.js
@@ -1,5 +1,4 @@
 // maneras de ubicar elementos
-
     var txtFrase = document.getElementById('txtFrase');
         txtFrase.innerText = 'Elemento encontrado!'
 
@@ -9,3 +8,8 @@
         losI[2].style.backgroundColor = '#f1985e';
         losI[2].style.padding = '3px 6px'
         losI[2].style.border = '1px dashed #f60';
+
+    var resaltados = document.getElementsByClassName('resaltado');
+        console.log(resaltados);
+    // variamos color de fondo
+        resaltados[1].style.backgroundColor = '#e9b08a';
